cebc32e4206bc10231744c9096ec1c7a7dee1962
Sergei Alimov <8912652@gmail.com>
2018-09-11 22:26:36 +0300

    add diff-two-arrays

---
+++ diff-two-arrays.js
@@ -0,0 +1,6 @@
+const diffArray = (arr1, arr2) => {
+  const newArr = arr2.filter((x) => !arr1.includes(x));
+  return newArr.concat(arr1.filter((x) => !arr2.includes(x)));
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
